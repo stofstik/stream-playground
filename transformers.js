b56a18abf8bf71fd1ff426c88d9774ef9928aeb9
stofstik <verstegen.daan@gmail.com>
2017-02-05 18:34:41 +0100

    added comments

--- transformers.js
+++ transformers.js
@@ -1,5 +1,8 @@
 const { Transform } = require('stream');
 
+/*
+ * Lolzify object if data > 0.5
+ */
 class Lolzify extends Transform {
   constructor(source, options = {}) {
     options.readableObjectMode = true;
@@ -39,4 +42,10 @@ class Stringify extends Transform {
   }
 }
 
-module.exports = { Lolzify, Stringify }
+/*
+ * This works, but lets use some es6 object destructuring : )
+ */
+// module.exports.Lolzify   = Lolzify;
+// module.exports.Stringify = Stringify;
+
+module.exports = { Lolzify, Stringify };
